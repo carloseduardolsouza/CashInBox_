8ff5dcdb5a711d2b610a4e2390f6da5117c9b6fd
Carlos Eduardo Lourenço De Souza <97536308+carloseduardolsouza@users.noreply.github.com>
2025-04-22 18:35:39 -0300

    Pagina Home Criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
+
+//Telas
+import Home from "./screens/Home/Home";
 
 //Home
 import { GoHome } from "react-icons/go";
@@ -35,186 +38,215 @@ import { FaRegMoneyBillAlt } from "react-icons/fa";
 //Caixa
 import { FaCashRegister } from "react-icons/fa6";
 
-import  "./menu.css"
-import { BrowserRouter as Router, Link ,Route , Routes } from 'react-router-dom';
+import "./menu.css";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 function App() {
-    //Status do menu
-  const [home , setHome] = useState(true)
-  const [vendas , setVendas] = useState(false)
-  const [clientes , setClientes] = useState(false)
-  const [produtos , setProdutos] = useState(false)
-  const [estoque , setEstoque] = useState(false)
-  const [contasPagar , setContasPagar] = useState(false)
-  const [caixa , setCaixa] = useState(false)
-  const [configs , setConfigs] = useState(false)
+  //Status do menu
+  const [home, setHome] = useState(true);
+  const [vendas, setVendas] = useState(false);
+  const [clientes, setClientes] = useState(false);
+  const [produtos, setProdutos] = useState(false);
+  const [estoque, setEstoque] = useState(false);
+  const [contasPagar, setContasPagar] = useState(false);
+  const [caixa, setCaixa] = useState(false);
+  const [configs, setConfigs] = useState(false);
 
   //Define o tamanho e se o texto ira aparecer
-  const [windowWidth , setWindowWidth] = useState('45px')
-  const [windowDisplay , setWindowDisplay] = useState('none')
+  const [windowWidth, setWindowWidth] = useState("45px");
+  const [windowDisplay, setWindowDisplay] = useState("none");
 
   const style = {
-    display: windowDisplay
-  }
+    display: windowDisplay,
+  };
 
   //controla de forma geral como o menu deve se comportar
   const menuOpen = (params) => {
-    if(params == false) {
-      setWindowDisplay('none')
-      setWindowWidth('45px')
+    if (params == false) {
+      setWindowDisplay("none");
+      setWindowWidth("45px");
     }
 
-    if(params == true){
-      setWindowWidth('160px')
+    if (params == true) {
+      setWindowWidth("160px");
 
       setTimeout(() => {
-        setWindowDisplay('block')
-      }, 190) 
+        setWindowDisplay("block");
+      }, 190);
     }
-  }
+  };
 
   //verifica se o menu esta aberto ou fechado
   const VerificarStatusMenu = () => {
-    if (windowWidth == '45px') {
-      menuOpen(true)
-    } else (
-      menuOpen(false)
-    )
-  }
+    if (windowWidth == "45px") {
+      menuOpen(true);
+    } else menuOpen(false);
+  };
 
   return (
     <div className="App">
       <Router>
-      <div className="MenuLateralBoxArea" style={{width: windowWidth}}>
-            <div className="MenuLateralBox Outline" onClick={VerificarStatusMenu}>
-                <RiMenu2Fill className="iconsMenuLateral"/>
-            </div>
-            <Link to="/" className="MenuLateralBox" onClick={() => {
-                setVendas(false)
-                setHome(true)
-                setClientes(false)
-                setProdutos(false)
-                setEstoque(false)
-                setContasPagar(false)
-                setCaixa(false)
-                setConfigs(false)
-            }}>
-                {(home && <GoHomeFill className="iconsMenuLateral"/>) || (
-                    <GoHome className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>Home</p>
-            </Link>
-            <Link to="/vendas" className="MenuLateralBox" onClick={() => {
-                setVendas(true)
-                setClientes(false)
-                setHome(false)
-                setProdutos(false)
-                setEstoque(false)
-                setContasPagar(false)
-                setCaixa(false)
-                setConfigs(false)
-            }}>
-                {(vendas && <RiMoneyDollarCircleFill className="iconsMenuLateral"/>) || (
-                    <RiMoneyDollarCircleLine className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>Vendas</p>
-            </Link>
-            <Link to="/clientes" className="MenuLateralBox"  onClick={() => {
-                setVendas(false)
-                setClientes(true)
-                setProdutos(false)
-                setEstoque(false)
-                setHome(false)
-                setContasPagar(false)
-                setCaixa(false)
-                setConfigs(false)
-            }}>
-                {(clientes && <FaUser className="iconsMenuLateral"/>) || (
-                    <FaRegUser className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>clientes</p>
-            </Link>
-            <Link to="/produtos" className="MenuLateralBox" onClick={() => {
-                setVendas(false)
-                setClientes(false)
-                setProdutos(true)
-                setEstoque(false)
-                setHome(false)
-                setContasPagar(false)
-                setCaixa(false)
-                setConfigs(false)
-            }}>
-                {(produtos && <MdSell className="iconsMenuLateral"/>) || (
-                    <MdOutlineSell className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>produtos</p>
-            </Link>
-            <Link to="/estoque" className="MenuLateralBox" onClick={() => {
-                setVendas(false)
-                setClientes(false)
-                setProdutos(false)
-                setEstoque(true)
-                setContasPagar(false)
-                setHome(false)
-                setCaixa(false)
-                setConfigs(false)
-            }}>
-                {(estoque && <BsBox2Fill className="iconsMenuLateral"/>) || (
-                    <BsBox2 className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>Estoque</p>
-            </Link>
-            <Link to="/contasPagar" className="MenuLateralBox" onClick={() => {
-                setVendas(false)
-                setClientes(false)
-                setProdutos(false)
-                setHome(false)
-                setEstoque(false)
-                setContasPagar(true)
-                setCaixa(false)
-                setConfigs(false)
-            }}>
-                {(contasPagar && <FaMoneyBill1 className="iconsMenuLateral"/>) || (
-                    <FaRegMoneyBillAlt className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>Contas a pagar</p>
-            </Link>
-            <Link to="/caixa" className="MenuLateralBox" onClick={() => {
-                setVendas(false)
-                setClientes(false)
-                setProdutos(false)
-                setEstoque(false)
-                setContasPagar(false)
-                setHome(false)
-                setCaixa(true)
-                setConfigs(false)
-            }}>
-                {(caixa && <FaCashRegister className="iconsMenuLateral"/>) || (
-                    <FaCashRegister className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>Caixa</p>
-            </Link>
+        <div className="MenuLateralBoxArea" style={{ width: windowWidth }}>
+          <div className="MenuLateralBox Outline" onClick={VerificarStatusMenu}>
+            <RiMenu2Fill className="iconsMenuLateral" />
+          </div>
+          <Link
+            to="/"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(false);
+              setHome(true);
+              setClientes(false);
+              setProdutos(false);
+              setEstoque(false);
+              setContasPagar(false);
+              setCaixa(false);
+              setConfigs(false);
+            }}
+          >
+            {(home && <GoHomeFill className="iconsMenuLateral" />) || (
+              <GoHome className="iconsMenuLateral" />
+            )}
+            <p style={style}>Home</p>
+          </Link>
+          <Link
+            to="/vendas"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(true);
+              setClientes(false);
+              setHome(false);
+              setProdutos(false);
+              setEstoque(false);
+              setContasPagar(false);
+              setCaixa(false);
+              setConfigs(false);
+            }}
+          >
+            {(vendas && (
+              <RiMoneyDollarCircleFill className="iconsMenuLateral" />
+            )) || <RiMoneyDollarCircleLine className="iconsMenuLateral" />}
+            <p style={style}>Vendas</p>
+          </Link>
+          <Link
+            to="/clientes"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(false);
+              setClientes(true);
+              setProdutos(false);
+              setEstoque(false);
+              setHome(false);
+              setContasPagar(false);
+              setCaixa(false);
+              setConfigs(false);
+            }}
+          >
+            {(clientes && <FaUser className="iconsMenuLateral" />) || (
+              <FaRegUser className="iconsMenuLateral" />
+            )}
+            <p style={style}>clientes</p>
+          </Link>
+          <Link
+            to="/produtos"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(false);
+              setClientes(false);
+              setProdutos(true);
+              setEstoque(false);
+              setHome(false);
+              setContasPagar(false);
+              setCaixa(false);
+              setConfigs(false);
+            }}
+          >
+            {(produtos && <MdSell className="iconsMenuLateral" />) || (
+              <MdOutlineSell className="iconsMenuLateral" />
+            )}
+            <p style={style}>produtos</p>
+          </Link>
+          <Link
+            to="/estoque"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(false);
+              setClientes(false);
+              setProdutos(false);
+              setEstoque(true);
+              setContasPagar(false);
+              setHome(false);
+              setCaixa(false);
+              setConfigs(false);
+            }}
+          >
+            {(estoque && <BsBox2Fill className="iconsMenuLateral" />) || (
+              <BsBox2 className="iconsMenuLateral" />
+            )}
+            <p style={style}>Estoque</p>
+          </Link>
+          <Link
+            to="/contasPagar"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(false);
+              setClientes(false);
+              setProdutos(false);
+              setHome(false);
+              setEstoque(false);
+              setContasPagar(true);
+              setCaixa(false);
+              setConfigs(false);
+            }}
+          >
+            {(contasPagar && <FaMoneyBill1 className="iconsMenuLateral" />) || (
+              <FaRegMoneyBillAlt className="iconsMenuLateral" />
+            )}
+            <p style={style}>Contas a pagar</p>
+          </Link>
+          <Link
+            to="/caixa"
+            className="MenuLateralBox"
+            onClick={() => {
+              setVendas(false);
+              setClientes(false);
+              setProdutos(false);
+              setEstoque(false);
+              setContasPagar(false);
+              setHome(false);
+              setCaixa(true);
+              setConfigs(false);
+            }}
+          >
+            {(caixa && <FaCashRegister className="iconsMenuLateral" />) || (
+              <FaCashRegister className="iconsMenuLateral" />
+            )}
+            <p style={style}>Caixa</p>
+          </Link>
 
-            <Link to="/configurações" className="MenuLateralBox Preferencias" onClick={() => {
-                setVendas(false)
-                setClientes(false)
-                setProdutos(false)
-                setEstoque(false)
-                setHome(false)
-                setContasPagar(false)
-                setCaixa(false)
-                setConfigs(true)
-            }}>
-                {(configs && <BsGearFill  className="iconsMenuLateral"/>) || (
-                    <BsGear className="iconsMenuLateral"/>
-                    )}
-                <p style={style}>Preferencias</p>
-            </Link>
-            <div className="MenuLateralBox">
-            </div>
+          <Link
+            to="/configurações"
+            className="MenuLateralBox Preferencias"
+            onClick={() => {
+              setVendas(false);
+              setClientes(false);
+              setProdutos(false);
+              setEstoque(false);
+              setHome(false);
+              setContasPagar(false);
+              setCaixa(false);
+              setConfigs(true);
+            }}
+          >
+            {(configs && <BsGearFill className="iconsMenuLateral" />) || (
+              <BsGear className="iconsMenuLateral" />
+            )}
+            <p style={style}>Preferencias</p>
+          </Link>
+          <div className="MenuLateralBox"></div>
         </div>
         <Routes>
-          <Route path="/" Component={``}/>
+          <Route path="/" Component={Home} />
         </Routes>
       </Router>
     </div>
---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,179 @@
+import "./Home.css";
+import { useState, useEffect } from "react";
+
+//Biblioteca de Gráficos
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+//Icones Usados
+import { FaTruck } from "react-icons/fa";
+import { GiTakeMyMoney } from "react-icons/gi";
+import { MdAttachMoney } from "react-icons/md";
+import { FaComputer } from "react-icons/fa6";
+import { IoNotifications } from "react-icons/io5";
+import { IoMdArrowDropup } from "react-icons/io";
+
+//componente que faz ligação com a Api
+//import fetchapi from "../../api/fetchapi";
+
+function Home() {
+  const [notificaçãp, setNotificação] = useState(false);
+  const [receitas, setReceitas] = useState();
+
+  //informações dos gráficos
+  const data = [
+    { name: "Jan", Despesas: 4000, Receitas: 2400 },
+    { name: "Fev", Despesas: 3000, Receitas: 1398 },
+    { name: "Mar", Despesas: 2000, Receitas: 9800 },
+    { name: "Abr", Despesas: 2780, Receitas: 3908 },
+    { name: "Mai", Despesas: 1890, Receitas: 4800 },
+    { name: "Jun", Despesas: 2390, Receitas: 3800 },
+    { name: "Jul", Despesas: 3490, Receitas: 4300 },
+  ];
+
+  /*useEffect(() => {
+    fetchapi.métricas().then((response) => setReceitas(response.receita))
+  }, [])*/
+
+  return (
+    <div id="Homescreen">
+      <div className="NotificationHomeScreen" onClick={() => ""}>
+        <IoNotifications />
+      </div>
+
+      <h1>{"CashInBox"}</h1>
+
+      <header className="HeaderHomeDeashBoard">
+        <LineChart
+          width={550}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Receitas" stroke="#31c331" />
+          <Line type="monotone" dataKey="Despesas" stroke="#de2727" />
+        </LineChart>
+
+        <div className="ButtonHeaderDeashBoard">
+          <button onClick={() => (window.location.href = "/")}>
+            <GiTakeMyMoney /> Funcionários
+          </button>
+          <button onClick={() => (window.location.href = "/")}>
+            <FaTruck /> Fornecedores
+          </button>
+          <button onClick={() => (window.location.href = "/")}>
+            <MdAttachMoney /> Fluxo de Caixa
+          </button>
+          <button onClick={() => (window.location.href = "/")}>
+            <FaComputer /> PDV
+          </button>
+        </div>
+
+        <div className="LoyautCardMétricasBox">
+          <article className="cardMétricasBox green">
+            <h2>Receitas</h2>
+            <h1>{}</h1>
+            <div className="linha" />
+            <div className="displayFlex">
+              <div>
+                <p>Ultimo més</p>
+                <strong>{"R$ 00,00"}</strong>
+              </div>
+              <div>
+                <p>
+                  <IoMdArrowDropup />
+                </p>
+                <strong>{"0%"}</strong>
+              </div>
+            </div>
+          </article>
+
+          <article className="cardMétricasBox red">
+            <h2>Despesas</h2>
+            <h1>{"R$ 00,00"}</h1>
+            <div className="linha" />
+            <div className="displayFlex">
+              <div>
+                <p>Ultimo més</p>
+                <strong>{"R$ 00,00"}</strong>
+              </div>
+              <div>
+                <p>
+                  <IoMdArrowDropup />
+                </p>
+                <strong>{"0%"}</strong>
+              </div>
+            </div>
+          </article>
+
+          <article className="cardMétricasBox orange">
+            <h2>Vendas Online</h2>
+            <h1>{"R$ 00,00"}</h1>
+            <div className="linha" />
+            <div className="displayFlex">
+              <div>
+                <p>Ultimo més</p>
+                <strong>{"R$ 00,00"}</strong>
+              </div>
+              <div>
+                <p>
+                  <IoMdArrowDropup />
+                </p>
+                <strong>{"0%"}</strong>
+              </div>
+            </div>
+          </article>
+
+          <article className="cardMétricasBox orange">
+            <h2>Vendas Presencial</h2>
+            <h1>{"R$ 00,00"}</h1>
+            <div className="linha" />
+            <div className="displayFlex">
+              <div>
+                <p>Ultimo més</p>
+                <strong>{"R$ 00,00"}</strong>
+              </div>
+              <div>
+                <p>
+                  <IoMdArrowDropup />
+                </p>
+                <strong>{"0%"}</strong>
+              </div>
+            </div>
+          </article>
+        </div>
+      </header>
+
+      <main className="MainHomeDeashBoard">
+        <article>
+          <h1>Vendas este mês</h1>
+          <h1 className="NotficationHome">{"0"}</h1>
+        </article>
+
+        <article>
+          <h1>Contas a pagar</h1>
+          <h1 className="NotficationHome">{"0"}</h1>
+        </article>
+
+        <article>
+          <h1>Vendas Pendentes</h1>
+          <h1 className="NotficationHome">{"0"}</h1>
+        </article>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
