f4afa2e38e0d1bc932415bf1da3dcb79a9319eaa
Carlos Eduardo Lourenço De Souza <97536308+carloseduardolsouza@users.noreply.github.com>
2025-04-25 00:15:18 -0300

    Cadastro de Funcionarios Feito

--- src/screens/CadastrarFuncionario/CadastrarFuncionario.jsx
+++ src/screens/CadastrarFuncionario/CadastrarFuncionario.jsx
@@ -1,11 +1,106 @@
-import "./CadastrarFuncionario.css"
+import "./CadastrarFuncionario.css";
+
+//icones
+import { FaUserAlt } from "react-icons/fa";
 
 function CadastrarFuncionario() {
-    return ( 
-        <div id="CadastrarFuncionario">
-            <h2>Novo Funcionário</h2>
-        </div>
-     );
+  return (
+    <div id="novoCliente">
+      <h2>Novo Funcionario</h2>
+      <p>{"data"}</p>
+      <div id="CENTRALIZAR">
+        <main className="MainNovoCliente">
+          <div alt="Imagem User" className="ImageUser">
+            <FaUserAlt />
+          </div>
+          <form className="articleNovoCliente">
+            <p>
+              <strong>Nome: </strong>
+            </p>
+            <input
+              type="text"
+              className="InputNovoCliente"
+              placeholder="Nome"
+              required
+            />
+            <p>
+              <strong>Numero: </strong>
+            </p>
+            <input
+              type="number"
+              className="InputNovoCliente"
+              placeholder="Numero"
+              required
+            />
+            <p>
+              <strong>Endereço</strong>
+            </p>
+            <input
+              type="text"
+              className="InputNovoCliente"
+              placeholder="Endereço"
+            />
+            <p>
+              <strong>CPF</strong>
+            </p>
+            <input
+              type="number"
+              className="InputNovoCliente"
+              placeholder="CPF"
+            />
+            <p>
+              <strong>Email</strong>
+            </p>
+            <input
+              type="text"
+              className="InputNovoCliente"
+              placeholder="Email"
+            />
+
+            <p>
+              <strong>Salario</strong>
+            </p>
+            <input
+              type="number"
+              className="InputNovoCliente"
+              placeholder="Salario em R$"
+            />
+            <div id="divSelectNovoFuncionario">
+              <div>
+                <p>
+                  <strong>Gênero</strong>
+                </p>
+                <select className="SelectNovoCliente">
+                  <option value="Selecione o Genero">Selecione o Genero</option>
+                  <option value="Masculino">Masculino</option>
+                  <option value="Feminino">Feminino</option>
+                </select>
+              </div>
+
+              <div>
+                <p>
+                  <strong>Cargo</strong>
+                </p>
+                <select className="SelectNovoCliente">
+                  <option value="Selecione o Genero">Selecione o Cargo</option>
+                  <option value="Vendedor">Vendedor</option>
+                  <option value="Gerente">Gerente</option>
+                  <option value="Entregador">Entregador</option>
+                  <option value="Caixa">Caixa</option>
+                </select>
+              </div>
+            </div>
+            <p>Nascimento: </p>
+            <input type="date" className="DataNovoCliente" />
+            <button className="CadastrarNovoCliente" type="submit">
+              Cadastrar
+            </button>
+            {/*concluido && <Concluindo/>*/}
+          </form>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default CadastrarFuncionario;
